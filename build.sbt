269ae746bb01c63baed9528f379ca98f1171a5cb
Raúl Piaggio <rpiaggio@gmail.com>
2019-04-10 02:16:41 -0300

    With debug.

--- build.sbt
+++ build.sbt
@@ -9,5 +9,6 @@ libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-stream" % "2.5.22",
   "com.beachape" %% "enumeratum" % "1.5.13",
   "org.scala-lang.modules" %% "scala-xml" % "1.1.1",
-  "org.jsoup" % "jsoup" % "1.11.3"
+  "org.jsoup" % "jsoup" % "1.11.3",
+  "io.github.howardjohn" %% "akka-http-lambda" % "0.3.1"
 )
--- src/main/scala/com/rpiaggio/feeder/WebServer.scala
+++ src/main/scala/com/rpiaggio/feeder/WebServer.scala
@@ -16,14 +16,13 @@ import org.jsoup.Jsoup
 
 import scala.collection.mutable
 import scala.concurrent.{ExecutionContext, Future}
-import scala.io.StdIn
+import io.github.howardjohn.lambda.akka.AkkaHttpLambdaHandler
 
 object WebServer {
   implicit private val system: ActorSystem = ActorSystem("feeder-system")
   implicit private val materializer: ActorMaterializer = ActorMaterializer()
   // needed for the future flatMap/onComplete in the end
   implicit private val executionContext: ExecutionContext = system.dispatcher
-
 
   private val maxRedirCount = 20
 
@@ -75,181 +74,254 @@ object WebServer {
 
   private val tickAntelEntryTemplate = FeedEntry("{%3} - {%2}", "{%1}", "{%4}")
 
-  def main(args: Array[String]) {
-    def requestWithRedirects(req: HttpRequest, count: Int = 0)(implicit system: ActorSystem, mat: Materializer): Future[HttpResponse] = {
+  def requestWithRedirects(req: HttpRequest, count: Int = 0)(implicit system: ActorSystem, mat: Materializer): Future[HttpResponse] = {
+//    println(s"Requesting [$req.uri]")
+    val result =
       Http().singleRequest(req).flatMap { resp =>
+//        println(s"Response [$resp]")
         resp.status match {
-          case StatusCodes.Found => resp.header[headers.Location].map { loc =>
-            val cookie = resp.header[headers.`Set-Cookie`].map(header => Cookie(header.cookie.pair))
-            val newReq = req.copy(uri = loc.uri, headers = req.headers ++ cookie)
-            if (count < maxRedirCount) requestWithRedirects(newReq, count + 1) else Http().singleRequest(newReq)
-          }.getOrElse(throw new RuntimeException(s"location not found on 302 for ${req.uri}"))
+          case StatusCodes.MovedPermanently | StatusCodes.Found | StatusCodes.SeeOther | StatusCodes.UseProxy | StatusCodes.TemporaryRedirect | StatusCodes.PermanentRedirect =>
+            resp.header[headers.Location].map { loc =>
+              val cookie = resp.header[headers.`Set-Cookie`].map(header => Cookie(header.cookie.pair))
+              val newReq = req.copy(uri = loc.uri, headers = req.headers ++ cookie)
+              if (count < maxRedirCount) requestWithRedirects(newReq, count + 1) else Http().singleRequest(newReq)
+            }.getOrElse(throw new RuntimeException(s"location not found on redirect for ${req.uri}"))
           case _ => Future(resp)
         }
       }
+
+    result.onFailure { case t: Throwable =>
+      t.printStackTrace()
     }
 
+    result
+  }
 
-    class EntryParserGraphStage(pattern: ParsePattern) extends GraphStage[FlowShape[ByteString, EntryData]] {
-      val input = Inlet[ByteString]("EntryParser.in")
-      val output = Outlet[EntryData]("EntryParser.out")
 
-      override def shape: FlowShape[ByteString, EntryData] = FlowShape(input, output)
+  class EntryParserGraphStage(pattern: ParsePattern) extends GraphStage[FlowShape[ByteString, EntryData]] {
+    val input = Inlet[ByteString]("EntryParser.in")
+    val output = Outlet[EntryData]("EntryParser.out")
 
-      override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = new GraphStageLogic(shape) {
+    override def shape: FlowShape[ByteString, EntryData] = FlowShape(input, output)
 
-        var entryRemainingInstructions: Option[Seq[ParseInstruction]] = None
+    override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = new GraphStageLogic(shape) {
 
-        var currentEntry: EntryData = Seq.empty[String]
+      var entryRemainingInstructions: Option[Seq[ParseInstruction]] = None
 
-        var entryQueue = mutable.Queue.empty[EntryData]
+      var currentEntry: EntryData = Seq.empty[String]
 
-        var previousBuffer: ByteString = ByteString.empty
+      var entryQueue = mutable.Queue.empty[EntryData]
 
-        setHandler(input, new InHandler {
-          override def onPush(): Unit = {
-            val bytes = previousBuffer ++ grab(input)
+      var previousBuffer: ByteString = ByteString.empty
 
-            def parseChunk(lastIndex: Int = 0): Unit = {
-              val nextString = entryRemainingInstructions.fold(pattern.start)(_.head.until)
-              val nextIndex = bytes.indexOfSlice(nextString.getBytes(StandardCharsets.UTF_8), lastIndex)
+      setHandler(input, new InHandler {
+        override def onPush(): Unit = {
+          val bytes = previousBuffer ++ grab(input)
 
-              if (nextIndex >= 0) {
-                entryRemainingInstructions.fold {
-                  entryRemainingInstructions = Some(pattern.instructions)
-                  parseChunk(nextIndex + pattern.start.length)
-                } { instructions =>
-                  val currentInstruction = instructions.head
-                  if (currentInstruction.action == ParseAction.Capture) {
-                    currentEntry = currentEntry :+ new String(bytes.slice(lastIndex, nextIndex).toArray, StandardCharsets.UTF_8).replaceAll("\n\r", "")
-                  }
+          def parseChunk(lastIndex: Int = 0): Unit = {
+            val nextString = entryRemainingInstructions.fold(pattern.start)(_.head.until)
+            val nextIndex = bytes.indexOfSlice(nextString.getBytes(StandardCharsets.UTF_8), lastIndex)
 
-                  val remainingInstructions = instructions.tail
-                  if (remainingInstructions.isEmpty) {
-                    if (isAvailable(output)) push(output, currentEntry) else entryQueue.enqueue(currentEntry)
-                    entryRemainingInstructions = None
-                    currentEntry = Seq.empty[String]
-                  } else {
-                    entryRemainingInstructions = Some(remainingInstructions)
-                  }
-
-                  parseChunk(nextIndex + currentInstruction.until.length)
+            if (nextIndex >= 0) {
+              entryRemainingInstructions.fold {
+                entryRemainingInstructions = Some(pattern.instructions)
+                parseChunk(nextIndex + pattern.start.length)
+              } { instructions =>
+                val currentInstruction = instructions.head
+                if (currentInstruction.action == ParseAction.Capture) {
+                  currentEntry = currentEntry :+ new String(bytes.slice(lastIndex, nextIndex).toArray, StandardCharsets.UTF_8).replaceAll("\n\r", "")
                 }
-              } else {
-                previousBuffer = bytes.drop(lastIndex)
+
+                val remainingInstructions = instructions.tail
+                if (remainingInstructions.isEmpty) {
+                  if (isAvailable(output)) push(output, currentEntry) else entryQueue.enqueue(currentEntry)
+                  entryRemainingInstructions = None
+                  currentEntry = Seq.empty[String]
+                } else {
+                  entryRemainingInstructions = Some(remainingInstructions)
+                }
+
+                parseChunk(nextIndex + currentInstruction.until.length)
               }
+            } else {
+              previousBuffer = bytes.drop(lastIndex)
             }
-
-            parseChunk()
-            if (!hasBeenPulled(input)) pull(input)
           }
-        })
 
-        setHandler(output, new OutHandler {
-          override def onPull() = {
-            if (entryQueue.nonEmpty) {
-              push(output, entryQueue.dequeue)
-            }
-            if (!hasBeenPulled(input)) pull(input)
+          parseChunk()
+          if (!hasBeenPulled(input)) pull(input)
+        }
+      })
+
+      setHandler(output, new OutHandler {
+        override def onPull() = {
+          if (entryQueue.nonEmpty) {
+            push(output, entryQueue.dequeue)
           }
-        })
-      }
-
+          if (!hasBeenPulled(input)) pull(input)
+        }
+      })
     }
 
-    def entryCreator(entryTemplate: FeedEntry, baseUri: Uri): Flow[EntryData, FeedEntry, Any] = Flow.fromFunction { seq =>
-      val parameterPattern = "\\{%(\\d+)\\}".r
+  }
 
-      def replace(template: String): String = {
-        parameterPattern.replaceAllIn(template, mtch => seq(mtch.group(1).toInt - 1))
-      }
+  def entryCreator(entryTemplate: FeedEntry, baseUri: Uri): Flow[EntryData, FeedEntry, Any] = Flow.fromFunction { seq =>
+    val parameterPattern = "\\{%(\\d+)\\}".r
 
-      FeedEntry(
-        Jsoup.parse(replace(entryTemplate.title)).body.text,
-        Uri(replace(entryTemplate.link)).resolvedAgainst(baseUri).toString,
-        replace(entryTemplate.description)
-      )
+    def replace(template: String): String = {
+      parameterPattern.replaceAllIn(template, mtch => seq(mtch.group(1).toInt - 1))
     }
 
-    val rssRender: Flow[FeedEntry, ByteString, Any] = Flow.fromFunction { entry =>
-      val node =
+    FeedEntry(
+      Jsoup.parse(replace(entryTemplate.title)).body.text,
+      Uri(replace(entryTemplate.link)).resolvedAgainst(baseUri).toString,
+      replace(entryTemplate.description)
+    )
+  }
+
+  val rssRender: Flow[FeedEntry, ByteString, Any] = Flow.fromFunction { entry =>
+    val node =
         <item>
           <title>{entry.title}</title>
           <link>{entry.link}</link>
           <description>{entry.description}</description>
         </item>
 
-      ByteString(node.toString + "\n", StandardCharsets.UTF_8)
-    }
+    ByteString(node.toString + "\n", StandardCharsets.UTF_8)
+  }
 
-    final case class Feed(channelEntry: FeedEntry, parsePattern: ParsePattern, entryTemplate: FeedEntry) {
-      lazy val parser = Flow.fromGraph(new EntryParserGraphStage(parsePattern))
-      lazy val formatter = entryCreator(entryTemplate, channelEntry.uri)
-    }
+  final case class Feed(channelEntry: FeedEntry, parsePattern: ParsePattern, entryTemplate: FeedEntry) {
+    lazy val parser = Flow.fromGraph(new EntryParserGraphStage(parsePattern))
+    lazy val formatter = entryCreator(entryTemplate, channelEntry.uri)
+  }
 
-    val tickAntelMusicaFeed = Feed(FeedEntry("TickAntel Música", "https://tickantel.com.uy/inicio/buscar_categoria?cat_id=1", "TickAntel Música"), tickAntelParsePattern, tickAntelEntryTemplate)
-    val tickAntelTeatroFeed = Feed(FeedEntry("TickAntel Teatro", "https://tickantel.com.uy/inicio/buscar_categoria?cat_id=2", "TickAntel Teatro"), tickAntelParsePattern, tickAntelEntryTemplate)
+  val tickAntelMusicaFeed = Feed(FeedEntry("TickAntel Música", "https://tickantel.com.uy/inicio/buscar_categoria?cat_id=1", "TickAntel Música"), tickAntelParsePattern, tickAntelEntryTemplate)
+  val tickAntelTeatroFeed = Feed(FeedEntry("TickAntel Teatro", "https://tickantel.com.uy/inicio/buscar_categoria?cat_id=2", "TickAntel Teatro"), tickAntelParsePattern, tickAntelEntryTemplate)
 
-    val feeds = Map(
-      "tickantel-musica" -> tickAntelMusicaFeed,
-      "tickantel-teatro" -> tickAntelTeatroFeed,
+  val feeds = Map(
+    "tickantel-musica" -> tickAntelMusicaFeed,
+    "tickantel-teatro" -> tickAntelTeatroFeed,
+  )
+
+  def prefix(channelEntry: FeedEntry) =
+    ByteString(
+      "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+        "<?xml-stylesheet type=\"text/xsl\" href=\"../xsl\"?>\n" +
+        "<rss version=\"2.0\">\n" +
+        "<channel>\n" +
+        <title>{channelEntry.title}</title>.toString +
+        <link>{channelEntry.link}</link>.toString +
+        <description>{channelEntry.description}</description>.toString + "\n",
+      StandardCharsets.UTF_8
     )
 
-    def prefix(channelEntry: FeedEntry) =
-      ByteString(
-        "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
-          "<?xml-stylesheet type=\"text/xsl\" href=\"/xsl\"?>\n" +
-          "<rss version=\"2.0\">\n" +
-          "<channel>\n" +
-          <title>{channelEntry.title}</title>.toString +
-          <link>{channelEntry.link}</link>.toString +
-          <description>{channelEntry.description}</description>.toString + "\n",
-        StandardCharsets.UTF_8
-      )
+  val suffix =
+    ByteString(
+      "</channel>\n" +
+        "</rss>",
+      StandardCharsets.UTF_8
+    )
 
-    val suffix =
-      ByteString(
-        "</channel>\n" +
-          "</rss>",
-        StandardCharsets.UTF_8
-      )
-
-    val xmlRoute =
-      path("feed" / Segment) { feedName =>
-        get {
-          feeds.get(feedName).fold(failWith(new Exception(s"Unknown feed [$feedName]"))) { feed =>
-            complete(
-              requestWithRedirects(HttpRequest(uri = feed.channelEntry.uri))
-                .transform {
-                  _.map { response =>
-                    HttpResponse(
-                      // headers = List(headers.`Content-Type`(MediaTypes.`application/rss+xml`.withCharset(HttpCharsets.`UTF-8`))),
-                      entity = response.entity.transformDataBytes(
-                        feed.parser.via(feed.formatter).via(rssRender)
-                          .prepend(Source(List(prefix(feed.channelEntry))))
-                          .concat(Source(List(suffix)))
-                          .log("Feeder")
-                      ).withContentType(ContentTypes.NoContentType)
-                      //.withContentType(MediaTypes.`application/rss+xml`.withCharset(HttpCharsets.`UTF-8`))
-                    )
-                  }
+  val xmlRoute =
+    path("feed" / Segment) { feedName =>
+      get {
+        feeds.get(feedName).fold(complete(s"Unknown stream [$feedName].")) { feed =>
+          complete(
+            requestWithRedirects(HttpRequest(uri = feed.channelEntry.uri))
+              .transform {
+                _.map { response =>
+                  HttpResponse(
+                    // headers = List(headers.`Content-Type`(MediaTypes.`application/rss+xml`.withCharset(HttpCharsets.`UTF-8`))),
+                    entity = response.entity.transformDataBytes(
+                      feed.parser.via(feed.formatter).via(rssRender)
+                        .recover { case t: Throwable => ByteString(t.getMessage + "\n" + t.getStackTrace.mkString("\n")) }
+                        .prepend(Source(List(prefix(feed.channelEntry))))
+                        .concat(Source(List(suffix)))
+                        .log("Feeder")
+                    ).withContentType(ContentTypes.NoContentType)
+                    //.withContentType(MediaTypes.`application/rss+xml`.withCharset(HttpCharsets.`UTF-8`))
+                  )
                 }
-            )
-          }
+              }
+          )
         }
       }
+    }
 
-    val xslRoute =
-      path("xsl") {
-        getFromResource("preview.xsl")
+  val debugRoute1 =
+    path("debug1") {
+      get {
+        val feed = feeds.head._2
+        complete(
+          requestWithRedirects(HttpRequest(uri = feed.channelEntry.uri))
+            .transform {
+              _.map { response => HttpResponse(entity = response.entity) }
+            }
+        )
       }
+    }
 
-    val cssRoute =
-      path("css") {
-        getFromResource("style.css")
+  val debugRoute2 =
+    path("debug2") {
+      get {
+        val feed = feeds.head._2
+        complete(
+          requestWithRedirects(HttpRequest(uri = feed.channelEntry.uri))
+            .transform {
+              _.map { response =>
+                HttpResponse(
+                  entity = response.entity
+                    .transformDataBytes(
+                      feed.parser.via(Flow.fromFunction(s => ByteString(s.toString)))
+                        .recover { case t: Throwable => ByteString(t.getMessage + "\n" + t.getStackTrace.mkString("\n")) }
+                        .log("Feeder")
+                    ).withContentType(ContentTypes.NoContentType)
+                )
+              }
+            }
+        )
       }
+    }
 
-    val bindingFuture = Http().bindAndHandle(xmlRoute ~ xslRoute ~ cssRoute, "localhost", 8080)
+  val debugRoute3 =
+    path("debug3") {
+      get {
+        val feed = feeds.head._2
+        complete(
+          requestWithRedirects(HttpRequest(uri = feed.channelEntry.uri))
+            .transform {
+              _.map { response =>
+                HttpResponse(
+                  entity = response.entity
+                    .transformDataBytes(
+                      feed.parser.via(feed.formatter).via(Flow.fromFunction(s => ByteString(s.toString)))
+                        .recover { case t: Throwable => ByteString(t.getMessage + "\n" + t.getStackTrace.mkString("\n")) }
+                        .log("Feeder")
+                    ).withContentType(ContentTypes.NoContentType)
+                )
+              }
+            }
+        )
+      }
+    }
+
+  val xslRoute =
+    path("xsl") {
+      getFromResource("preview.xsl")
+    }
+
+  val cssRoute =
+    path("css") {
+      getFromResource("style.css")
+    }
+
+  val router = xmlRoute ~ xslRoute ~ cssRoute ~ debugRoute1 ~ debugRoute2 ~ debugRoute3
+
+
+  def main(args: Array[String]) {
+    import scala.io.StdIn
+
+    val bindingFuture = Http().bindAndHandle(router, "localhost", 8080)
 
     println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
@@ -257,5 +329,7 @@ object WebServer {
       .flatMap(_.unbind()) // trigger unbinding from the port
       .onComplete(_ => system.terminate()) // and shutdown when done
   }
+
+  class EntryPoint extends AkkaHttpLambdaHandler(router)
 
 }
