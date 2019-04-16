55b0d05e2fbbc188f5a6a5f100aa3e93e7c5147d
Raúl Piaggio <rpiaggio@gmail.com>
2019-04-15 23:41:57 -0300

    Abitab feeds. But only first page, we need some multi-page logic.

--- src/main/scala/com/rpiaggio/feeder/AllFeeds.scala
+++ src/main/scala/com/rpiaggio/feeder/AllFeeds.scala
@@ -1,5 +1,5 @@
 package com.rpiaggio.feeder
 
 object AllFeeds extends FeedList {
-  val feeds = uy.TickAntel.feeds ++ uy.RedUTS.feeds
+  val feeds = uy.TickAntel.feeds ++ uy.RedUTS.feeds ++ uy.Abitab.feeds
 }
--- src/main/scala/com/rpiaggio/feeder/uy/RedUTS.scala
+++ src/main/scala/com/rpiaggio/feeder/uy/RedUTS.scala
@@ -3,17 +3,6 @@ package com.rpiaggio.feeder.uy
 import com.rpiaggio.feeder._
 
 object RedUTS extends FeedList {
-  private val parsePattern1 = ParsePattern(
-    """<article{*}
-      |<a href="{%}"{*}
-      |<div class="event-date-day" date="">{%}</div>{*}
-      |<div class="event-date-month text-uppercase" date="">{%}</div>{*}
-      |<div class="event-title">{%}</div>{*}
-      |<div class="event-venue">{%}</div>{*}
-      |<div class="event-hour">{%}</div>{*}
-      |</article>""".stripMargin
-  )
-
   private val parsePattern = ParsePattern(
     """<article{*}
       |<a href="{%}"{*}
@@ -25,7 +14,6 @@ object RedUTS extends FeedList {
       |</article>""".stripMargin
   )
 
-  private val entryTemplate1 = FeedEntry("{%4} - {%3} {%2} - {%6}", "{%1}", "{%5}")
   private val entryTemplate = FeedEntry("{%3} - {%2} - {%5}", "{%1}", "{%6}<br/>{%4}")
 
   private def buildFeed(title: String, url: String) =
