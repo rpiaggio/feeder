0d434ed878a6597ce9147ad4781e8df70584191b
Raúl Piaggio <rpiaggio@gmail.com>
2019-04-18 16:36:32 -0300

    Agrego noticias DSM.

--- src/main/scala/com/rpiaggio/feeder/AllFeeds.scala
+++ src/main/scala/com/rpiaggio/feeder/AllFeeds.scala
@@ -1,5 +1,5 @@
 package com.rpiaggio.feeder
 
 object AllFeeds extends FeedList {
-  val feeds = uy.TickAntel.feeds ++ uy.RedUTS.feeds ++ uy.Abitab.feeds
+  val feeds = uy.TickAntel.feeds ++ uy.RedUTS.feeds ++ uy.Abitab.feeds ++ uy.DSM.feeds
 }
